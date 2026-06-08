d6d28bf628c0b9fb3b7125e980b6da7654a73610
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-08 11:32:46 +0530

    Create variables_data_tyes.py

---
+++ variables_data_tyes.py
@@ -0,0 +1,127 @@
+
+# Variables : Are containers help us to store data.
+
+# x = 10
+
+# name = "programography"
+
+# age = 25
+
+# when creating a variable:
+    
+#     Rules:
+#         1) Veriables always start from charcters or underscore (_)
+
+
+# a = 10
+
+# hello = 200
+
+# Abc = 20
+
+# _a = 10
+
+# a1 = 50
+
+# 2) Variables never be start from integer
+
+# 1a = 10
+
+# 11 = "hello"
+
+
+# 3) Variables never hold spaces
+
+
+# my name = "programography"
+
+# my_name = "hello"
+
+# 4) Variables never hold spacial symbols
+
+# !@#$%^&*(()*&^%$#@!@!@#)
+
+# x! = 100
+
+# 5) Vairble never be a keyword
+
+
+# q_0 = "hello"
+
+# _001 = "hey"
+
+# __ = 20
+
+# print(__)
+
+
+
+# keyword : are spacial words that have there own functionalty.
+
+
+# break = 10
+
+# if = 10
+
+
+# if, for, while, range, def, break, continue, .....
+
+
+
+# Data Types:
+#     1) Integer
+#         -12, 34, 45, 0, 5, -1000
+        
+#     2) Float
+#         10.0, 1.0, 2.4, 100.1
+        
+#     3) String
+#         'hello'
+#         "hey"
+#         """how are you"""
+#         '''ok'''
+    
+    
+#         "10"
+#         '10.0'
+    
+    
+#     4) Boolean
+#         True, False
+
+#     5) Complex
+#         a + bj
+        
+#         10 + 20j
+        
+# Spacial Data Types:
+#     1) List
+#     2) Tuple
+#     3) Set
+#     4) Dictionry
+    
+
+# # x = 10
+# # name = "programography"
+
+#  1) List
+# #  []
+
+x = "helo"
+ 
+
+# print(x)
+
+print(type(x))
+
+
+
+
+8219836118
+
+
+
+
+
+
+
