46c66e2aa2ece9a78d86745560e3743174197c8a
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-09 17:39:41 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,113 @@
+# Variabls
+# data types
+
+# Comments:
+
+# adding two numbers
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+# Types of comments:
+#     1) Single line comments
+#     2) Multi line comments
+
+
+#     1) Single line comments
+
+# a = 10
+
+'''
+a = 10
+b = 20
+
+c = 30
+'''
+
+#     2) Multi line comments
+
+
+# akjsbdkas
+# dajdbakbdasd
+# asjdbksabd
+
+# ctrl + /
+
+# 1000 
+
+
+# adding two number
+
+# a = 10  # storing the data in variable
+
+# b = 20  # stortig data in varable b
+
+# c = a + b # adding data
+
+# #showing the data
+# print(c)
+
+
+
+# ---------------------------------------
+
+# Core functions:
+#     print()
+#     input()
+#     type()
+#     count()
+#     len()
+
+
+
+# hello()
+
+# x()
+
+# print()
+
+
+
+
+
+# print()
+#     ---> help us to print anything in output.
+
+# print("Hello how are you")
+
+# print("how are you")
+
+# print('heyyyyy')
+
+
+# print("""!!!!1111""")
+
+
+# "" or '' --> used only to print single line.
+
+
+# """""" / '''''' --> help to print multiline string
+
+# print("""Namre : programogray
+# age : 20
+# phone number : 819861919""")
+
+
+
+# x = "hello"
+
+# y = 10000
+
+# print(x, y, x, x, "ok")
+
+
+x = 10
+
+y = 20
+
+
+
