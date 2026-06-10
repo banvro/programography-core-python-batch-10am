950fb9f6e30ab89411559f9c6eb93eb01f4f7e69
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-10 12:09:59 +0530

    Create input function and type casting.py

---
+++ input function and type casting.py
@@ -0,0 +1,165 @@
+# a = 10
+
+# b = 20
+
+# c = a + b
+
+# print("the sum of", a, "and", b, "is : ", c)
+
+
+
+name = "programography"
+
+age = 25
+
+phone_number = 8219836118
+
+address = "sector 34 a chandigarh"
+
+
+# print("The coaching instteute name is", name, "and the age is", age, "and for contact with instetute we have contact number is", phone_number, "At", address)
+
+# f-string (formatted string)
+
+# name = "programography"
+# age = 25
+# phone_number = 8219836118
+# address = "sector 34 a chandigarh"
+
+
+# print(f"The coaching instteute name is {name}, and the age is {age} and for contact with instetute we have contact number is {phone_number} At {address}")
+
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(f"the sum of {a} and {b} is {c}")
+
+
+
+# input()
+#     ---> help us to get input from user
+
+
+
+# a = 20
+# b = 30
+
+# x = a + b
+
+# print(x)
+
+
+
+# name = input("Enter your name : ")
+
+# print("user entred name : ", name)
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+
+# print(f"User name is {name} and age is {age}.")
+
+
+# x = input("Enter first number : ")
+# y = input("Enter second number : ")
+
+# zx = x + y
+
+# print(f"The sum of {x} and {y} is {zx}.")
+
+
+# when we take input using input function, the data is always a string
+
+
+# x = input("Enter first number : ")
+# y = input("Enter seocnd number : ")
+
+# xy = x + y
+
+# print("The sum of", x, "and", y, "is :", xy)
+
+
+# x = "1"
+# y = "2"
+
+# zx = x + y
+
+# print(zx)
+
+# concatenation : joinign two strings is called concatenation.
+
+
+
+# Type Casting : the process to conver data from one data type to another data type.
+
+
+# string ---> int()  ---- for integer
+# integer ---> float() --- for dasimcal data
+
+# str
+# int
+# float
+# bool
+# complex
+# list
+# set
+# tuple
+# dict
+
+
+# x = 10
+
+# print(x, type(x))
+
+# y = float(x)
+
+# print(y, type(y))
+
+
+
+
+# x = "100"
+
+# print(x, type(x))
+
+
+# q = int(x)
+
+# print(q, type(q))
+
+
+# x = input("Enter first number : ")
+# y = input("Enter seocnd number : ")
+
+# xy = x + y
+
+# print("The sum of", x, "and", y, "is :", xy)
+
+
+
+x = int(input("Enter first number : "))
+
+y = int(input("ENter second number : "))
+
+w = x + y
+
+w = float(w)
+
+print(w)
+
+
+
+
+# x = 1
+# y = 3
+
+# print(x + y)
+
+
+# x = 10
+
+
