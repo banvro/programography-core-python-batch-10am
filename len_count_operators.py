87e9a1512bbdb1e2f9779bcab428b33009ed94f1
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-11 12:11:22 +0530

    Create len_count_operators.py

---
+++ len_count_operators.py
@@ -0,0 +1,177 @@
+# Objects:
+#     1) Ittrable objects
+#         string, list, tuple, set, dictionry
+    
+#     2) Non ittrable objects
+#         integer, float, boolen, complex
+        
+# len(), count(), loops --------> ittrable objects.        
+
+# len()
+#     ---> help us to know, the total number of elemnts in an ittrable objects.
+
+# zx = "hello hi"
+
+# print(len(zx))
+
+
+# x = 1234544
+
+# print(len(x))
+
+
+
+# c = 102730129730217302173021730123702173
+
+# x = str(c)
+
+
+# print(len(x))
+
+
+
+# count()
+#     ---> help to know about, how many times a particuler element occur in a ittrable object.
+
+# x = "this is a car and this is good car."
+
+# print(x.count("a"))
+
+
+# task :
+    
+#     user_data = input("ENter your data : ")
+    
+#     what_to_find = "er"
+
+
+
+# x = input("Enter your data : ")
+
+# y = input("What count need to find : ")
+
+# print(x.count(y))
+
+
+
+# input()  x
+# "98"
+
+# a = 10
+# b = a * a + a
+# print(b - len(str(a)))
+
+
+# a = 10
+
+# b = a * a + a
+
+# z = int((a + a) / 10)
+
+# print(b - z)
+
+
+# x = 10
+
+# print(x ** 2)
+
+
+
+# print()
+# input()
+# type()
+# len()
+# count()
+
+# -------------------------------------------
+
+# Operators
+
+
+
+# 10 + 20
+
+# Operator --> +
+# opernds --> 10, 20
+
+
+# 1) Arithmetic Opeators:
+#     1) addation    ---> +
+#     2) Subtration  ----> -
+#     3) Multiplication ---> *
+#     4) Division -----> /
+#     5) Modules  ------>  %
+#     6) Floor devsion ---> //
+#     7) Exponentional ---> **
+
+
+
+# print(10 + 20)
+
+# print(100 - 34)
+
+# print(10 * 3)
+
+# print("hello " * 50)
+
+
+
+# print(7 / 2)
+
+# print(7 // 2)
+
+
+# print(10 % 3)
+
+
+# print(10 % 2)
+
+# print(2 ** 4)
+
+# Compersion Operators
+    # ---> True / False
+
+    
+    # 1) == 
+
+# print(10 == 10)
+
+# 2) != (not equal to)
+
+
+# print(10 != 3)
+
+# 3) >
+
+# print(34 > 34)
+
+# ==
+# !=
+# >
+# >=
+# <
+# <=
+
+
+# print(10 >= 10)
+
+
+# ----------------------------------
+
+# Assignment Operator
+
+# 1) =
+# 2) +=
+# 3) -=
+# 4) /=
+
+a = 10
+
+# a = a + 20
+
+a += 20
+
+print(a)
+
+
+
