4ef4f046232a584217fd0a944d6cf6fb871df583
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-12 11:50:39 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,122 @@
+# Operators:
+#     1) Airthamtical Opeators
+#     2) Compersion Operators
+#     3) Assignment Operators
+    
+#     4) Logical Operators
+#         i) and
+#         ii) or
+#         iii) not
+
+
+# age = 20
+# c = "india"
+
+# print(age >= 18 and c == "India")
+
+# *
+
+# 0    0   |  0
+# 0    1   |  0
+# 1    0   |  0
+# 1    1   |  1
+
+
+
+# 0    0   |  0
+# 0    1   |  1
+# 1    0   |  1
+# 1    1   |  1
+
+# age = 20
+# c = "india"
+
+# print(age >= 18 or c == "india")
+
+
+
+# not
+
+# 0  |  1
+# 1  |  0
+
+
+# print(not(10 == 10))
+
+
+
+
+
+# Mambership Operators:
+    # ---> True, False
+    # to check wether a data exist in a sequence
+# 1) in
+# 2) not in
+
+
+# zx = "this is a car and this is a train"
+
+# print("plane" in zx)
+
+
+
+
+# zx = 1092379120730
+
+# print(10 in zx)
+
+
+# lst = (12, 34, 45, 56, 6, 8)
+
+# print(45 in lst)
+
+
+
+
+# zx = "this is a car and this is a train"
+
+# print("plane" not in zx)
+
+
+# 7. Identity Operators
+
+# is, is not
+
+
+# a = 10
+
+# b = 20
+
+
+# print(a is not b)
+
+
+
+
+
+
+# Bitwise Operators:
+
+# a = 10
+
+# b = 89
+
+# print(a & b)
+
+# &   
+
+# or   |
+
+# not   ~
+
+
+# print(~10)
+
+# -(x + 1)
+
+
+
+# print(~2)
+
+
+
