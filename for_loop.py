cbf5350211ae53961de7dacc9aa8e3abc2a79482
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-16 11:47:22 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,238 @@
+# Loops:
+#     ittration / repetation
+    
+#     1) for loop
+#     2) while loop
+
+
+# 1) for loop
+
+# for verible_name in object:
+#     # block of code
+
+
+# x = "this is a car"
+
+# for i in x:
+#     print(i)
+#     print("helloooo")
+#     print(i, i, i, i, i)
+
+# print("done..........")
+
+
+
+
+# t = "hello"
+
+# hello = "hey"
+
+# for x in t:
+#     print(hello)
+
+
+
+
+# for loop =---> work only on itrable objects
+
+
+# x = 1000230304
+
+# x = str(x)
+
+# for q in x:
+#     print(q)
+
+
+
+
+
+# xyz = "1230023000300120"
+
+# for p in xyz:
+#     print(xyz)
+
+
+
+# x = "10034400020003004001"
+
+# for i in x:
+    
+#     if i == '0':
+#         print("hello")
+    
+#     else:
+#         print(i)
+
+
+# 1 ---> hey
+# 0 ---> hello
+# 2 ---> ok
+
+# abc = "123145000120002001201222"
+
+# for i in abc:
+#     if i == "1":
+#         print("hey")
+    
+#     elif i == "0":
+#         print('hello')
+    
+#     elif i == 2:
+#         print("ok")
+    
+#     else:
+#         print(i)
+
+
+# range()
+#     --> help us to genrate data from one location to another.
+
+
+# range(start, end, increment)
+
+# default:
+    
+#     start --- 0
+#     end ---> n - 1
+#     increment --- 1
+
+
+# for i in range(3, 12, 2):
+#     print(i)
+
+
+
+
+
+
+# for i in range(4, 10):
+#     print(i)
+
+
+
+
+
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+
+# for i in range(10):
+#     print("hellooo")
+
+
+
+
+# for i in range(1, 11):
+#     print(i * 2)
+
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# ..
+# .
+# .
+# .
+
+# 2 x 10 = 20
+
+
+# for i in range(1, 11, 1):
+#     # print("2 *", i, "=", 2*i)
+    
+#     print(f"14 x {i} = {14*i}")
+
+
+# 130 x 4 = 7
+# 129 x 5 = 14
+# 128 x 6 = 21
+# 127 x 7 = 28
+# 126 x 8 = 35
+# .
+# .
+# .
+# .
+#          = 70
+
+
+# for i in range(1, 11):
+#     print(131 - i, "x", 3 + i, "=", 7*i)
+
+
+# 2 x 1 = 2
+# 3 x 2 = 6
+# 2 x 3 = 8
+# 3 x 4 = 12
+# 2 x 5 = 10
+# 3 x 6 = 18
+# .
+# .
+# .  x 10 = 
+
+
+# for i in range(1, 11):
+    
+#     if i % 2 == 0:
+#         print(f"3 x {i} = {3*i}")
+#     else:
+#         print(f"2 x {i} = {2*i}")
+        
+
+# print(4 % 2)
+
+
+
+
+1
+2 2
+3 3 3 
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+7 7 7 7 7 7 7
+8 8 8 8 8 8 8 8
+9 9 9 9 9 9 9 9 9
+
+
+
+fizzbuzz
+
+1 ---- 50
+
+3 --- fizz
+5 --- buzz
+3, 5 ---> fizzbuzz
+
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+16
+
+
+
+
+
+
+
+
+
+
