3f878553909332c3fd362f1d1fad183d1b9d19f2
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-19 11:30:10 +0530

    Create while_ture.py

---
+++ while_ture.py
@@ -0,0 +1,173 @@
+# # 0  a
+# # 1  b             a
+# # 1  c = a + b     b
+# # 2   c = a + b
+# # 3
+# # 5
+# # 8
+# # 13
+# # 21
+# # 34
+# # 55
+# # 89
+
+
+# # a = 0
+# # b = 1
+
+# # while a < 90:
+# #     print(a)
+# #     c = a + b  # 1, 2, 3
+# #     a = b  # 1, 1, 2
+# #     b = c  # 1, 2, 3
+    
+    
+
+# Python Properties:
+#     1) Simple
+#     2) General purpose
+#     3) High Level Language
+#     4) Object Oriented (OOPs)
+#     5) Interpreated
+    
+    
+    
+
+# Langauges
+# 1) High Level Langauge
+# 2) Low level langauge
+#     Machine Understandable
+#         --> Binary Langauge (0, 1)
+    
+    
+
+# Programming Language
+#     --> Compiler Based
+#     -->  Interpreated based
+    
+# C / C++
+    
+    
+    
+    
+# print("helloooo")
+
+# print("ok")
+
+
+# no
+
+# print("done")
+    
+
+# Python Properties:
+#     1) Simple
+#     2) General purpose
+#     3) High Level Language
+#     4) Object Oriented (OOPs)
+#     5) Interpreated
+#     6) Scripting Langauge
+#     7) Dynlically typed 
+#     8) Case Senstive
+    
+    
+# a = 10
+
+# A = 20
+
+# print(a)
+    
+    
+# SQL
+
+
+# While Condation:
+#     code.........
+
+
+# 3 statments
+    # 1) break
+    # 2) continue
+    # 3) pass
+
+
+# for i in range(1, 11):
+    
+#     print(i)
+    
+#     if i == 3:
+#         break
+
+
+
+
+# x = 1
+
+# while x < 5:
+    
+#     print(x)
+    
+#     x = x + 1
+    
+    # break
+    
+    
+    
+# continue
+
+
+# for i in range(1, 11):
+    
+#     if i == 5:
+#         continue
+
+#     print(i)
+
+
+# pass
+
+# for i in range(2):
+#     pass
+
+    
+
+# while condation:   
+
+
+# while True:
+    
+
+# import random
+
+# print(random.randint(1, 90))
+    
+
+while True:
+    print()
+    chocie = input("Press q for quit and s for start : ")
+    print()
+    if chocie == "q":
+        print("Thank you for using..")
+        break
+    
+    elif chocie == "s":
+        num1 = int(input("Enter first number : "))
+        num2 = int(input("Enter second number : "))
+        
+        zx = num1 + num2
+        
+        print(f"the sum of {num1} and {num2} is : {zx}")
+        print()
+    else:
+        print("Enter valid option..")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
