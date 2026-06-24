5c328454b7093181aa7f7736dc1417695af97219
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-24 11:35:21 +0530

    Create list_ds.py

---
+++ list_ds.py
@@ -0,0 +1,229 @@
+# Varibales
+# Data types
+# Type Casting
+# Core functions
+# condational statement
+# Loops
+# control statments
+
+# Data Structure
+
+# x = 100
+
+# name = "hello"
+
+# List
+# tuple
+# set
+# dictionry
+
+
+# a = 10
+
+# b = 20
+
+# a, b = 10, 20
+
+# print(a, b)
+
+
+
+# List
+#     --> help to store collection of data.
+    
+#     []
+
+# ---> seprated with ,
+
+fruits = ["apple", "banana", "pineapple", "orange"]
+
+
+# print(fruits)
+
+# print(type(fruits))
+
+
+# Homogenious
+#     --> same type of data..
+
+# Hitrogenious
+#     --> diffrent type of data
+
+
+# [10, 34, 45, 56, 23, 4, 3, 5, 2]
+
+
+# x = ["a", 10, 10.0, True, "hello"]
+
+# print(x)
+
+
+# ---> Ordered
+# ---> allow duplpicate data
+# ---> Mutable (modify)
+
+
+# indexing / slicing
+
+
+# x = [1, 34, 5, 67, 78, 9, "hey", 10000]
+
+# print(x[3])
+
+# print(x[-2])
+
+# print(x[-1])
+
+
+# x = [1, 34, 5, 67, 78, 9, "hey", 10000]
+
+# print(x[2 : 7])
+
+
+
+
+# xy = [10, 34, 10, 3242, 456]
+
+# print(xy)
+
+# Modify ---> new data, delete, replace
+
+
+
+# x = [12, 34, 45, 6, 10, 32]
+
+# print(x)
+
+# adding new elemens:
+#     append()
+#         ---> add element at last
+#     insert()
+
+
+# x.append(120)
+
+# print(x + 10)
+
+# zx = []
+
+# x = [12, 34, 45, 6, 10, 32]
+
+# for i in x:
+#     zx.append(i + 10)
+
+# print(zx)
+
+
+
+# xy = "hello"
+
+# y = []
+
+# for i in xy:
+#     y.append(i)
+    
+# print(y)
+
+
+
+
+# [1, 22, 333, 44444, 55555, 666666, 7777777, 88888888]
+
+# zx = []
+
+# for i in range(1, 9):
+#     zx.append(int(i * str(i)))
+
+# print(zx)
+
+
+
+# 1 ---- 50
+
+# odd = []
+# even = []
+
+
+
+# odd = []
+# even = []
+
+# for i in range(1, 51):
+#     if i % 2 == 0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+
+# print(odd)
+# print(even)
+
+
+
+# 2) insert()
+
+# zx = [12, 34, 5, 6, 73, 23]
+
+# zx.insert(3, 1000)
+
+# print(zx)
+
+
+# Deleting elements
+
+# pop()
+# remove()
+
+# zx = [12, 34, 5, 6, 73, 23]
+
+# # zx.pop()
+# # zx.pop()
+
+# zx.remove(34)
+
+
+# print(zx)
+
+
+
+
+
+
+# x = [12, 4, 5, 6]
+
+# y = [100, 4000, 344, 5, 3]
+
+# print(x + y)
+
+# x.extend(y)
+
+# print(x)
+
+
+
+
+
+# tuple:
+#     --> ordered
+#     --> allow duplicate data
+#     ---> imutable
+    
+#     ()
+
+
+
+# zx = (12, 34, 45, 56, 67)
+
+# print(zx[2])
+
+
+
+
+['2 x 10 = 20', '2 x 9 = 18', '2 x 8 = 16', .....]
+
+
+
+
+
+
+
+
