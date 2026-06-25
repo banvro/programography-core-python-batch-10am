5b8f6469a2812e3c712e8cb3240ee5707e775990
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-25 12:19:43 +0530

    Create dict_.py

---
+++ dict_.py
@@ -0,0 +1,181 @@
+# list
+# tuple
+
+
+# set:
+    # ---> unordered
+#     ---> do not allow duplicate data
+#     ---> mutable
+    
+#     {}
+    
+    
+# zx = {12, 34, "hello", 34, "hello", 5, 6, 12}
+
+# print(zx)
+    
+# mutable
+
+# zx = {12, 34, 5, 6}
+
+# adding new element
+
+# add()
+
+# zx.add(3)
+
+# remove elemnt
+# zx.remove(34)
+# zx.remove(6)
+
+# print(zx)
+    
+    
+# zx = {12, 1, 3, 5}
+
+# zx.remove(100)
+
+# zx.discard(100)
+
+# zx.pop()
+
+# print(zx)    
+    
+    
+    
+    
+    
+# zx = {12, 1, 3, 5}    
+
+
+# # update()
+
+# x = "[98, 76, 54, 34]"
+
+# zx.update(x)
+    
+    
+# print(zx)
+    
+    
+    
+    
+    
+# print(zx.intersection(pq))
+
+# print(zx.union(pq))
+# zx = {12, 34, 56, 6}
+
+# pq = {12, 4, 56, 23}
+
+
+# print(pq.difference(zx))
+
+
+
+# zx = {12, 34, 56, 6}
+
+# zx.clear()
+
+# print(zx)
+
+
+
+
+# zx = [12, 34, 5, 45]
+
+
+# zx.clear()
+
+# print(zx)
+
+
+# x = [12]
+
+# print(type(x))
+    
+# c = (12)
+
+# print(type(c))
+    
+    
+    
+    
+# Dictionry
+    # --> ordered
+    # --> do not allow duplicate data
+    # --> mutable
+    
+    # {}
+    
+
+# zx = set()
+
+# print(type(zx))
+    
+    
+    
+    
+    
+    
+#  key : value
+
+# user_ = {
+#     "name" : "naresh", 
+#     "age" : 25, 
+#     "email" : "naresh@gmail.com",
+# }
+    
+# print(user_["number"])
+
+# print(user_.get("number"))
+
+# user_.pop("age")
+# user_.pop("name")
+
+# user_.popitem()
+    
+# print(user_)
+    
+    
+    
+    
+# user_ = {
+#     "name" : "naresh", 
+#     "age" : 25, 
+#     "email" : "naresh@gmail.com",
+# }
+
+# print(user_.keys())
+
+# print(user_.values())
+
+# print(user_.items())
+
+    
+# user_ = {
+#     "name" : "naresh", 
+#     "age" : 25, 
+#     "email" : "naresh@gmail.com",
+# }    
+
+# # user_["number"] = 8219836118
+
+# p = {
+#     "number" : 8219836118,
+#     "address" : "this is address"
+# }
+    
+    
+# user_.update(p)
+
+# print(user_)
+
+    
+    
+    
+    
+    
+    
+    
+    
