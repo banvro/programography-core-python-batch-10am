a1294259a22c126943373e9df4d4d3058b8c76c9
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-27 12:30:52 +0530

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,66 @@
+# While Loop:
+#     # ittrations  / repetetion
+#     ---> condation
+
+# while condation:
+    # code
+    
+# while 10 == 10:
+#     print("helloooooooo")
+
+
+# a = 1
+
+# while a < 10:
+    
+#     a = a + 1
+    
+#     print(a)
+
+# x = "3"
+
+# while x == 3:
+    
+#     print("doneeee")
+
+
+# x = 20
+
+
+# [0, 1, 1, 2, 3, 5, 8, 13]
+
+
+
+
+# a = 0
+# b = 1
+
+# lst = []
+
+# inp = int(input("Enter a  number : "))
+
+# while a < inp:
+#     lst.append(a)
+    
+#     c = a + b
+#     a = b
+#     b = c
+
+# print(lst)
+
+for i in range(6, 0, -1):
+    print(" " * (6 - i) + i * "* ")
+
+for i in range(1, 7):
+    print(" " * (6 - i) + (i) * "* ")
+
+
+
+
+
+
+
+
+
+
+    
