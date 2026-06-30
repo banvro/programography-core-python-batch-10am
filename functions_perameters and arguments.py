89733b66836db65d8f5862d2e0280cbf3059f658
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-30 12:01:39 +0530

    Create functions_perameters and arguments.py

---
+++ functions_perameters and arguments.py
@@ -0,0 +1,113 @@
+# functions:
+    
+# def xyz():
+#     code.....
+#     das
+#     d
+#     as
+#     as
+
+
+
+# def helo():
+#     print("helloooo")
+#     print("helloooo")
+#     print("helloooo")
+    
+    
+    
+# helo() 
+    
+# helo()
+    
+    
+    
+    
+# def greet():
+#     print("Hey Naresh!, Good morning")
+#     print("how are you!")
+    
+    
+# greet()
+    
+    
+# greet()
+
+
+
+# Parameters / Arguments
+
+# Parameters : 
+    
+# def xyz(a):
+#     print("helloooo", a)
+    
+
+# arguments
+# xyz(100) 
+
+
+# xyz("ok")
+    
+
+# def addd(a, b):
+#     print(f"the sum of {a} and {b} is : {a + b}")
+    
+    
+# addd("eeee", "oooooo") 
+    
+    
+# table(10)
+    
+    
+    
+    
+    
+    
+    
+# def table(x):
+    
+#     for i in range(1, 11):
+#         print(x, "x", i, "=", x*i)
+
+
+# table(34)
+
+
+
+# function
+#     --->
+#     "this is a car"
+
+
+
+
+# def xyzz(r):
+    
+#     zx = {}
+    
+#     for i in str(r):
+#         zx[int(i)] = str(r).count(i)
+    
+#     print(zx)
+
+# xyzz(111111111199999222)
+
+# {1 : 1, 2 : 1, 0 : 2, 3 : 2, 4 : 3}
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
